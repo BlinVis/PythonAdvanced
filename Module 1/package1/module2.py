a322bf567fe303c111362a3f6f521b4f76c0f52a
BlinVis <blin_visoka@hotmail.com>
2024-10-18 17:00:52 +0200

    added first file

---
+++ Module 1/package1/module2.py
@@ -0,0 +1,20 @@
+emri="Blin"
+dogi='Haski'
+tmetat=0
+
+print(type(emri))
+emri_mbiemri= emri + ' Visoka'
+print(emri_mbiemri)
+mylist=[16,1,2,3,4,['Blin','Visoka',17]]
+
+emri= mylist[5][0]
+print(emri)
+#append veq nfund tlistes, per me shtu te ni ven i caktum e perdor insert
+mylist.insert(3,'Cookie')
+print(mylist)
+mylist.remove(16)
+#te funksioni remove duhesh me shkru emrin e elementit e jo indeksin
+
+print(mylist)
+
+
